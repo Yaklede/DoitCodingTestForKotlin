29f3cb5db34868c73e561320e237738e9f4e16c6
Yaklede <chh78151@naver.com>
2023-04-21 09:38:39 +0900

    feature: 0번 문제 해결완료 백준 2750

--- codingTestForKotlin/src/main/kotlin/com.solve/day1/NumberSorting.kt
+++ codingTestForKotlin/src/main/kotlin/com.solve/day1/NumberSorting.kt
@@ -1,4 +1,20 @@
 package day1
 
-class NumberSorting {
+import common.Solution
+
+class NumberSorting: Solution {
+    override fun solution() {
+        val num = readln().toInt()
+        val sorting = mutableListOf<Int>()
+        val stringBuilder = StringBuilder()
+        for (i: Int in 0 until num) {
+            sorting.add(readln().toInt())
+        }
+
+        sorting.sort()
+        sorting.map {
+            stringBuilder.append("$it\n")
+        }
+        println(stringBuilder.toString())
+    }
 }
